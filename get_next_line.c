fb30668c199e9c13ab5a8795023826034d708210
Hyacine Alnuma <halnuma@z1r10p1.42lyon.fr>
2024-11-28 16:01:37 +0100

    passing all std paco tests norm ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: halnuma <halnuma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:30:01 by halnuma           #+#    #+#             */
-/*   Updated: 2024/11/28 14:59:33 by halnuma          ###   ########.fr       */
+/*   Updated: 2024/11/28 16:01:06 by halnuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*get_next_line(int fd)
 	int			nl;
 	char		*line;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || !fd)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	nl = 0;
 	line = NULL;
@@ -31,49 +31,6 @@ char	*get_next_line(int fd)
 	}
 	if (!nl)
 		line = next_line(fd, line, buffer, nl);
-	return (line);
-}
-
-char	*shift_buffer(char *buffer, int i)
-{
-	int	j;
-
-	j = 0;
-	while (buffer[i])
-	{
-		buffer[j] = buffer[i];
-		i++;
-		j++;
-	}
-	while (buffer[j])
-	{
-		buffer[j] = '\0';
-		j++;
-	}
-	return (buffer);
-}
-
-char	*process_buffer(char *line, char *buffer)
-{
-	int	i;
-
-	i = 0;
-	while (buffer[i])
-	{
-		if (buffer[i] == '\n')
-		{
-			line = join_line(line, buffer, (i + 1));
-			if (!line)
-				return (NULL);
-			buffer = shift_buffer(buffer, i + 1);
-			return (line);
-		}
-		i++;
-	}
-	line = join_line(line, buffer, (i + 1));
-	if (!line)
-		return (NULL);
-	buffer = shift_buffer(buffer, i + 1);
 	return (line);
 }
 
@@ -114,18 +71,57 @@ char	*next_line(int fd, char *line, char *buffer, int nl)
 	return (line);
 }
 
+char	*process_buffer(char *line, char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+		{
+			line = join_line(line, buffer, (i + 1));
+			if (!line)
+				return (NULL);
+			buffer = shift_buffer(buffer, i + 1);
+			return (line);
+		}
+		i++;
+	}
+	line = join_line(line, buffer, (i + 1));
+	if (!line)
+		return (NULL);
+	buffer = shift_buffer(buffer, i + 1);
+	return (line);
+}
+
+char	*shift_buffer(char *buffer, int i)
+{
+	int	j;
+
+	j = 0;
+	while (buffer[i])
+	{
+		buffer[j] = buffer[i];
+		i++;
+		j++;
+	}
+	while (buffer[j])
+	{
+		buffer[j] = '\0';
+		j++;
+	}
+	return (buffer);
+}
+
 // int	main(int ac, char**av)
 // {
 // 	int fd;
+// 	char	*line;
 
 // 	(void)ac;
 // 	(void)av;
 // 	fd = open(av[1], O_RDONLY);
-// 	//fd = 0;
-// 	if (fd < 0)
-// 	{
-// 		write(2, "File error\n", 11);
-// 		return (0);
-// 	}
-// 	get_next_line(fd);
+// 	line = get_next_line(0);
+// 	printf("%s", line);
 // }
